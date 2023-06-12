76b83245c09001f97c8359148d33403a70332864
Ayush Goyal <ayushgoyal8178@gmail.com>
2023-06-13 01:54:53 +0530

    start building

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+
+export{
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard
+}
